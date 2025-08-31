cdd35db5e3424f4bfdf5d87cad6270796c4012b2
chrisjohnson11835-sys <chrisjohnson11835@gmail.com>
2025-08-31 14:39:15 -0400

    Delete uploader.py

--- uploader.py
+++
@@ -1,6 +0,0 @@
-import requests, os
-def post_file(url, secret, local_path, remote_dir="/public_html/data"):
-    with open(local_path,"rb") as f:
-        files={"file":(os.path.basename(local_path), f, "application/octet-stream")}
-        data={"secret":secret,"target_dir":remote_dir}
-        r=requests.post(url, data=data, files=files, timeout=60); r.raise_for_status(); return r.text
